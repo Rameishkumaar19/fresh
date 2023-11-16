28869911202500b0ab9e4587e6248b86e2454056
Rameishkumaar19 <kumarramez19@gmail.com>
2023-11-16 17:42:31 +0530

    new

---
+++ src/org/univ/College.java
@@ -0,0 +1,27 @@
+package org.univ;
+
+public class College extends University {
+	
+	 @Override
+	public void ug() {
+		 System.out.println("ug is undergraduate");
+		
+	}
+
+	@Override
+	public void pg() {
+		 System.out.println("pg is post graduate");
+		
+	}
+    public static void main(String[] args) {
+		College c=new College();
+		c.ug();
+		c.pg();
+    	
+    	
+    	
+	}
+	
+	
+	
+}
